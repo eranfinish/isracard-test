c2eaa577e932add84ac29dfd1683f72f49537e60
eranfinish <38463659+eranfinish@users.noreply.github.com>
2019-09-12 21:51:52 +0300

    Add files via upload

---
+++ gitApp/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public partial class _Default : System.Web.UI.Page
+{
+    public  static List<JObject> Bookmarked { get
+        {
+            //if(Session["bookmarks"] == null)
+            //{
+            //    Session["bookmarks"] = new List<JObject>();
+            //}
+            return HttpContext.Current.Session["bookmarks"] as List<JObject>;
+        }
+
+        set
+        {
+            value = HttpContext.Current.Session["bookmarks"] as List<JObject>;
+        }
+    }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    [WebMethod]
+    public static void BookmarkRepo(object obj)
+    {
+        var list = (List<JObject>)HttpContext.Current.Session["bookmarks"];
+        if(list == null)
+        {
+            list = new List<JObject>();
+
+        }
+        else
+        {
+            if(list.Exists(x=>x["id"] == (obj as JObject)["id"]))
+            {
+                list.Remove(x => x["id"] == (obj as JObject)["id"]);
+            }
+        }
+        list.Add( obj as JObject);
+       HttpContext.Current.Session["bookmarks"] = list;
+        //       HttpContext.Current.Response.Write(list.Count);// Bookmarked.Add(obj);
+    }
+
+    [WebMethod]
+    public static object GetBookmarks()
+    {
+        return Bookmarked;
+    }
+}
---
+++ gitApp/assets/js/index.js
@@ -0,0 +1,39 @@
+﻿var app = angular.module("gitApp", ['ui.router']);
+    app.controller("mainCtrl", function ($scope, $http) {
+    $http.get("https://api.github.com/search/repositories?q=YOUR_SEARCH_KEYWORD")
+        .then(function (response) {
+            console.log(response.data);
+            $scope.repos = response.data.items;
+            //console.log($scope.repos);
+        });
+
+    $scope.checkItem = function (item) {
+        //  item["check"] = item["check"] ? false : true;
+        console.log(item.check);
+        if (item.check) {
+            var config = {
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8"
+                }
+            };
+            var data = item;
+            //$http.post("Default.aspx/BookmarkRepo", JSON.stringify(data), config).then(function (response) {
+
+            //}
+            //);
+            $http({
+                url: "Default.aspx/BookmarkRepo",
+                method: "POST",
+                data: { "obj": data },
+                headers: { 'Content-Type': "application/json; charset=utf-8" }
+            }).then(function (response) {
+                
+            }
+            );
+        }
+    }
+
+
+}
+
+)
